0a715884e420d683e68d59d841630e8c100fb09a
thisislawatts <luke@thisis.la>
2016-01-12 23:36:23 +0000

    Adds pagination

--- home.php
+++ home.php
@@ -1,15 +1,23 @@
 <?php if ( have_posts() ): ?>
 	<div class="wrap">
-		<h1 class="page--title">Blog</h1>
-		<div class="posts">
-		<?php while ( have_posts() ) : the_post(); ?><!-- 
-		 --><article <?php post_class(); ?>>
-				<h1 class="h2"><?php the_title() ?></h1>
-				<time datetime="<?php echo get_the_date('c') ?>"><?php the_date() ?></time>
-				<?php the_excerpt() ?>
-				<a href="<?php the_permalink() ?>">Read more...</a>
-			</article><!-- 
-	 --><?php endwhile ?>
-		</div>
+		<h1 class="page--title"><a href="<?php echo get_permalink( get_option('page_for_posts') ); ?>">Blog</a></h1>
+		<?php if ( have_posts() ) : ?>
+			<div class="posts">
+			<?php while ( have_posts() ) : the_post(); ?><!-- 
+			 --><article <?php post_class(); ?>>
+					<header>
+						<h1 class="h2"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h1>
+						<time datetime="<?php echo get_the_date('c') ?>"><?php the_date() ?></time>
+					</header><!-- /header -->
+					<?php the_excerpt() ?>
+					<a href="<?php the_permalink() ?>">Read more...</a>
+				</article><!-- 
+		 --><?php endwhile ?>
+			</div>
+			<div class="posts"><?php the_posts_navigation(); ?></div>
+		<?php else : ?>
+		<?php endif; ?>
 	</div>
+
+
 <?php endif ?>
