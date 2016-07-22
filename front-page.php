d6c091c11f4c302df884b04bc11a278342285bf1
Luke Watts <luke@thisis.la>
2016-07-22 06:51:47 +0100

    Moves #intro into .wrap

--- front-page.php
+++ front-page.php
@@ -1,17 +1,17 @@
-<?php if ( have_posts() ) : ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-		<section id="intro">
-			<h1 class="page--title"><?php the_title() ?></h1>
-			<?php the_content() ?>
-			<?php wp_nav_menu( array(
-					'theme_location'	=> 'slms_home_links',
-					'container'			=> false,
-				) );
-			?>
-		</section>
-	<?php endwhile ?>
-<?php endif ?>
 <div class="wrap">
+	<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<section id="intro">
+				<h1 class="page--title"><?php the_title() ?></h1>
+				<?php the_content() ?>
+				<?php wp_nav_menu( array(
+						'theme_location'	=> 'slms_home_links',
+						'container'			=> false,
+					) );
+				?>
+			</section>
+		<?php endwhile ?>
+	<?php endif ?>
 	<div class="widgets">
 		<?php dynamic_sidebar( 'slms_home_sidebar' ); ?>
 	</div>
